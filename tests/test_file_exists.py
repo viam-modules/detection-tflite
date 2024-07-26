194b78fc0e31fb387e461b75c4c225cbff4d8f37
Tahiya Salam <tahiyasalam@gmail.com>
2024-07-26 15:20:29 -0400

    Add configurable param for num epochs

--- tests/test_file_exists.py
+++ tests/test_file_exists.py
@@ -3,4 +3,4 @@ import os
 
 def test_file_exists():
     assert os.path.exists("labels.txt")
-    assert os.path.exists("classification_model.tflite")
+    assert os.path.exists("detection_model.tflite")
