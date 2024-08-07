496f16f1060f68c1d2159628ad19691bdc51aaa2
Tahiya Salam <tahiyasalam@gmail.com>
2024-08-07 09:55:38 -0400

    Actually run test

--- tests/test_file_exists.py
+++ tests/test_file_exists.py
@@ -2,6 +2,5 @@ import os
 
 
 def test_file_exists():
-    print("hi")
-    # assert os.path.exists("labels.txt")
-    # assert os.path.exists("detection.tflite")
+    assert os.path.exists("labels.txt")
+    assert os.path.exists("detection.tflite")
