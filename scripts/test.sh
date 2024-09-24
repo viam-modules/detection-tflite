1320b99b5b386d6c74780ffa181b344d20f08034
Vignesh <vpandiarajan20@gmail.com>
2024-09-24 17:39:03 -0400

    change test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-cleanup() {
-    rm -f labels.txt detection.tflite
-}
-
 python3 setup.py sdist --formats=gztar
 pip install dist/model-0.1.tar.gz
 
 python3 -m model.training --dataset_file=dataset.jsonl --model_output_directory=. --num_epochs=2
 pip install pytest && pytest tests/test_file_exists.py
 
-cleanup()
+rm labels.txt
+rm detection.tflite
 
 python3 -m model.training --dataset_file=dataset.jsonl --model_output_directory=. --num_epochs=2 --labels=green_square/blue_star
 pytest tests/
