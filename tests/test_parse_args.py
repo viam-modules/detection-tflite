8e96eafa9b4c813320bdd48a5ea283469456d1aa
Vignesh <vpandiarajan20@gmail.com>
2024-09-26 16:14:09 -0400

    modify test

--- tests/test_parse_args.py
+++ tests/test_parse_args.py
@@ -3,16 +3,10 @@ import model.training
 
 def test_parse_args():
     args = [
-        "--dataset_file",
-        "test_data.json",
-        "--model_output_directory",
-        "test_output/",
-        "--num_epochs",
-        "5",
-        "--labels",
-        "label1",
-        "label2",
-        "label3",
+        "--dataset_file=test_data.json",
+        "--model_output_directory=test_output/",
+        "--num_epochs=5",
+        "--labels='label_1 label_2'"
     ]
 
     data_json, model_dir, num_epochs, labels = model.training.parse_args(args)
@@ -24,8 +18,4 @@ def test_parse_args():
         model_dir == "test_output/"
     ), f"Expected model_output_directory to be 'test_output/', got {model_dir}"
     assert num_epochs == 5, f"Expected num_epochs to be 5, got {num_epochs}"
-    assert labels == [
-        "label1",
-        "label2",
-        "label3",
-    ], f"Expected labels to be ['label1', 'label2', 'label3'], got {labels}"
+    assert labels == "'label_1 label_2'", f"Expected labels to be 'label_1 label_2', got {labels}"
